0128f19db0c0d0c772d4cbe3be66f6121d02c4d5
adk-saugat <adhikarisaugat34@gmail.com>
2024-11-15 10:55:12 -0600

    adding sign-in to the home page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,20 @@
 import { Link, Outlet } from "react-router-dom"
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
-import './navigation.styles.scss'
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
+import "./navigation.styles.scss"
 
 const Navigation = () => {
   return (
     <>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrwnLogo className="logo"/>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
           </Link>
         </div>
       </div>
